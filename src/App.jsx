c2589b6dd3c013a96d62cf596e262c8ecdc2846f
AnanTade123 <ananttade@education.opticalarc.com>
2024-07-29 18:12:12 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+
+import { Button } from '@material-tailwind/react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+   <Button>Hello</Button>
+    </>
+  )
+}
+
+export default App
